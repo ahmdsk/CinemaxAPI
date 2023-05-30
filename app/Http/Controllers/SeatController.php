89fd2b0d46b2bddb1ca05e23962aefdf04cfefdf
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-05-30 15:49:45 +0700

    add crud seat

--- app/Http/Controllers/SeatController.php
+++ app/Http/Controllers/SeatController.php
@@ -2,9 +2,97 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Seat;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class SeatController extends Controller
 {
-    //
+    public function index()
+    {
+        $seats = Seat::all();
+
+        if (count($seats) > 0) {
+            return response()->json([
+                'message' => 'Retrieve All Seat Success',
+                'data'    => $seats
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'No Seat Found',
+                'data'    => null
+            ], 404);
+        }
+    }
+
+    public function create(Request $request)
+    {
+        $seat = $request->only('seat_code');
+
+        $validator = Validator::make($seat, [
+            'seat_code'    => 'required',
+        ]);
+
+        //if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $create_seat = Seat::create($seat);
+        if ($create_seat) {
+            return response()->json([
+                'message'   => 'Create Seat Success',
+                'data'      => $seat
+            ], 200);
+        } else {
+            return response()->json([
+                'message'   => 'Create Seat Failed',
+                'data'      => null
+            ], 400);
+        }
+    }
+
+    public function update(Request $request)
+    {
+        $seat = $request->only('seat_code');
+
+        $validator = Validator::make($seat, [
+            'seat_code'    => 'required',
+        ]);
+
+        //if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $update_seat = Seat::where('id', $request->id)->update($seat);
+        if ($update_seat) {
+            return response()->json([
+                'message'   => 'Update Seat Success',
+                'data'      => $seat
+            ], 200);
+        } else {
+            return response()->json([
+                'message'   => 'Update Seat Failed',
+                'data'      => null
+            ], 400);
+        }
+    }
+
+    public function delete(Request $request) {
+        $seat = Seat::find($request->id);
+
+        if ($seat) {
+            $seat->delete();
+            return response()->json([
+                'message'   => 'Delete Seat Success',
+                'data'      => $seat
+            ], 200);
+        } else {
+            return response()->json([
+                'message'   => 'Delete Seat Failed, Seat Not Found!',
+                'data'      => null
+            ], 400);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\SeatController;
 use App\Http\Controllers\TicketsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -38,6 +39,12 @@ Route::get('/events', [EventController::class, 'index'])->name('events.index');
 Route::post('/events/create', [EventController::class, 'create'])->name('events.create');
 Route::put('/events/edit/{id}', [EventController::class, 'update'])->name('events.edit');
 Route::delete('/events/delete/{id}', [EventController::class, 'delete'])->name('events.delete');
+
+// Seat
+Route::get('/seats', [SeatController::class, 'index'])->name('seats.index');
+Route::post('/seats/create', [SeatController::class, 'create'])->name('seats.create');
+Route::put('/seats/edit/{id}', [SeatController::class, 'update'])->name('seats.edit');
+Route::delete('/seats/delete/{id}', [SeatController::class, 'delete'])->name('seats.delete');
 
 // Tickets
 Route::get('/tickets', [TicketsController::class, 'index'])->name('tickets.index');
